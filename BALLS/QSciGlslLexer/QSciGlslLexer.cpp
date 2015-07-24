f16a1d6b2346ceb3dac7759541ab0cb25e726f2b
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2015-07-23 20:39:59 -0400

    Implement some GLSL lexer code
    - I think
    - God, I hate QScintilla

--- BALLS/QSciGlslLexer/QSciGlslLexer.cpp
+++ BALLS/QSciGlslLexer/QSciGlslLexer.cpp
@@ -6,22 +6,22 @@
 
 #include <Qsci/qsciscintilla.h>
 
+template<class T, class U, class V>
+T clamp(T t, U min, V max) noexcept {
+  if (t < min) { return min; }
+
+  if (t > max) { return max; }
+
+  return t;
+}
+
+// TODO: Adopt the style used on https://www.opengl.org/wiki
+// See https://www.opengl.org/wiki/Core_Language_(GLSL)
 QSciGlslLexer::QSciGlslLexer(QOpenGLShader::ShaderType type,
                              QObject* parent) noexcept :
 QsciLexerCustom(parent),
                 _type(type)
 {
-  _styles[Style::Default] = {
-    -1,
-    "Keyword",
-    QColor(255, 0, 0),
-    QColor(0, 0, 255),
-    QFont("Monospace"),
-    true
-  };
-
-  std::fill(_styles.begin(), _styles.end(), _styles[0]);
-
 }
 
 QStringList QSciGlslLexer::autoCompletionWordSeparators() const {
@@ -32,6 +32,7 @@ const char* QSciGlslLexer::language() const {
   return constants::LANGUAGE;
 }
 
+/*
 const char* QSciGlslLexer::lexer() const {
   // TODO: Figure out why the hell I can't use ShaderType's QMetaEnum
   switch (_type) {
@@ -57,6 +58,16 @@ const char* QSciGlslLexer::lexer() const {
     return "";
   }
 }
+*/
+
+const char* QSciGlslLexer::keywords(int set) const
+{
+  if (set == 1) {
+    return ("in out varying uniform");
+  }
+
+  return (0);
+}
 
 int QSciGlslLexer::braceStyle() const {
   return Style::Brace;
@@ -78,33 +89,37 @@ const char* QSciGlslLexer::blockEnd(int* style) const {
   return constants::BLOCK_END_TOKENS;
 }
 
-int QSciGlslLexer::lexerId() const {
-  return _type;
-}
-
 const char* QSciGlslLexer::wordCharacters() const {
   return constants::WORD_CHARS;
 }
 
-QColor QSciGlslLexer::color(int style) const {
-  return _styles[0].color();
-}
+const char* QSciGlslLexer::blockStartKeyword(int* style) const
+{
+  if (style) {
+    *style = Style::Control;
+  }
 
-QFont QSciGlslLexer::font(int style) const {
-  return _styles[0].font();
+  return constants::BLOCK_START_KEYWORDS;
 }
 
 QString QSciGlslLexer::description(int style) const {
-
-  return _styles[0].description();
+  return styles()[clamp(style, 0, styles().size() - 1)].description();
 }
 
-QColor QSciGlslLexer::paper(int style) const {
-  return _styles[0].paper();
+QColor QSciGlslLexer::defaultColor(int style) const {
+  return styles()[clamp(style, 0, styles().size() - 1)].color();
 }
 
-bool QSciGlslLexer::eolFill(int style) const {
-  return _styles[0].eolFill();
+QColor QSciGlslLexer::defaultPaper(int style) const {
+  return styles()[clamp(style, 0, styles().size() - 1)].paper();
+}
+
+QFont QSciGlslLexer::defaultFont(int style) const {
+  return styles()[clamp(style, 0, styles().size() - 1)].font();
+}
+
+bool QSciGlslLexer::defaultEolFill(int style) const {
+  return styles()[clamp(style, 0, styles().size() - 1)].eolFill();
 }
 
 void QSciGlslLexer::styleText(int start, int end) {
@@ -113,24 +128,162 @@ void QSciGlslLexer::styleText(int start, int end) {
   QString text(range);
   delete range;
 
+  qDebug() << text;
+
   startStyling(start);
   QStringList lines = text.split('\n');
 
   for (int i = 0; i < lines.size(); i++) {
     int style = Default;
+    const QString& line = lines[i];
 
-    if (lines.at(i).startsWith("//")) {
+    if (line.startsWith("//")) {
       style = Comment;
     }
-
-    if (lines.at(i).startsWith("#")) {
-      style = Macro;
+    else if (line.startsWith("#")) {
+      style = Preprocessor;
     }
 
-    setStyling(lines.at(i).size(), style);
+    setStyling(line.size(), style);
 
     if (i < lines.size() - 1) {
       setStyling(1, Default);
     }
   }
+}
+
+const std::array<QsciStyle, QSciGlslLexer::Style::Count>&
+QSciGlslLexer::styles() noexcept {
+  static std::array<QsciStyle, Style::Count> _styles;
+  static bool initialized = false;
+
+  if (Q_UNLIKELY(!initialized)) {
+    initialized = true;
+
+    QFont monospace("Monospace");
+    monospace.setPointSize(9);
+    QFont bold(monospace);
+    QFont italic(monospace);
+    QFont strike(monospace);
+
+    bold.setBold(true);
+    italic.setItalic(true);
+    strike.setStrikeOut(true);
+
+    _styles[Style::Default] = {
+      Style::Default,
+      "Keyword",
+      QColor("#000000"),
+      constants::DEFAULT_PAPER,
+      monospace,
+      true
+    };
+
+    _styles[Style::Type] = {
+      Style::Type,
+      "Type",
+      QColor("#000066"),
+      constants::DEFAULT_PAPER,
+      bold,
+      true
+    };
+
+    _styles[Style::Specifier] = {
+      Style::Specifier,
+      "Specifier",
+      QColor("#333399"),
+      constants::DEFAULT_PAPER,
+      bold,
+      true
+    };
+
+    _styles[Style::Specifier] = {
+      Style::Property,
+      "Property",
+      QColor("#006600"),
+      constants::DEFAULT_PAPER,
+      monospace,
+      true
+    };
+
+    _styles[Style::Literal] = {
+      Style::Literal,
+      "Literal",
+      QColor("#0000ff"),
+      constants::DEFAULT_PAPER,
+      monospace,
+      true
+    };
+
+    _styles[Style::Brace] = {
+      Style::Brace,
+      "Brace",
+      QColor("#000066"),
+      constants::DEFAULT_PAPER,
+      monospace,
+      true
+    };
+
+    _styles[Style::Function] = {
+      Style::Function,
+      "Function",
+      QColor("#993333"),
+      constants::DEFAULT_PAPER,
+      bold,
+      true
+    };
+
+    _styles[Style::Operator] = _styles[Style::Brace];
+
+    _styles[Style::Comment] = {
+      Style::Comment,
+      "Comment",
+      QColor("#666666"),
+      constants::DEFAULT_PAPER,
+      italic,
+      true
+    };
+
+    _styles[Style::Predefined] = {
+      Style::Predefined,
+      "Predefined",
+      QColor("#551111"),
+      constants::DEFAULT_PAPER,
+      monospace,
+      true
+    };
+
+    _styles[Style::Preprocessor] = {
+      Style::Preprocessor,
+      "Preprocessor",
+      QColor("#009900"),
+      constants::DEFAULT_PAPER,
+      monospace,
+      true
+    };
+
+    _styles[Style::Control] = {
+      Style::Control,
+      "Control",
+      QColor("#000000"),
+      constants::DEFAULT_PAPER,
+      bold,
+      true
+    };
+
+    _styles[Style::Reserved] = {
+      Style::Reserved,
+      "Reserved",
+      QColor("#FF0000"),
+      QColor("#000000"),
+      strike,
+      true
+    };
+
+    _styles[Style::BlockComment] = _styles[Style::Comment];
+
+    qDebug() << "Initialized default GLSL styles";
+  }
+
+  return _styles;
 }
--- BALLS/QSciGlslLexer/QSciGlslLexer.hpp
+++ BALLS/QSciGlslLexer/QSciGlslLexer.hpp
@@ -17,42 +17,47 @@ public:
   explicit QSciGlslLexer(QOpenGLShader::ShaderType, QObject* parent = 0) noexcept;
 
   const char* language() const override;
-  const char* lexer() const override;
+  //const char* lexer() const override;
   int braceStyle() const override;
-  int lexerId() const override;
-  const char* blockStart(int *style) const override;
-  const char* blockEnd(int *style) const override;
-  QColor color(int style) const override;
-  QColor paper(int style) const override;
-  QFont font(int style) const override;
+  const char* blockStart(int* style) const override;
+  const char* blockEnd(int* style) const override;
+  const char* blockStartKeyword(int *style) const override;
+  QColor defaultColor(int style) const override;
+  QColor defaultPaper(int style) const override;
+  QFont defaultFont(int style) const override;
   QString description(int style) const override;
-  bool eolFill(int style) const override;
+  bool defaultEolFill(int style) const override;
 
   void styleText(int start, int end) override;
   QStringList autoCompletionWordSeparators() const override;
   const char* wordCharacters() const override;
+  const char* keywords(int set) const override;
 signals:
 
 public slots:
 private:
   enum Style {
-    Default,
-    Macro,
-    Keyword,
-    Brace,
-    Operator,
+    Default, // Whitespace
+    Type,
+    Specifier,
+    Property,
     Function,
     Literal,
-    Type,
+    Operator,
     Comment,
+    Brace,
+    Predefined,
+    Preprocessor,
+    Control,
     BlockComment,
     Reserved,
     Count,
   };
 
-  std::array<QsciStyle, Style::Count> _styles;
   QOpenGLShader::ShaderType _type;
   QMetaEnum _enum;
+
+  static const std::array<QsciStyle, Style::Count>& styles() noexcept;
 };
 
 #endif // QSCIGLSLLEXER_HPP
